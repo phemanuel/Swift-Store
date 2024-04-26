d18752651c65881c233d7ca0d7117af72636a10f
phemanuel <emmakinyooye@gmail.com>
2024-04-25 19:50:34 -0700

    First Commit

---
+++ database/migrations/2020_04_24_170630_create_customers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name', 20);
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('birth_day')->nullable();
+            $table->string('birth_month')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('client_id');
+            $table->string('avatar')->nullable();
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
